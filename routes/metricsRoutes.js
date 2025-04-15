0d310471f5d5cf2f764bd469377788bad47d4b83
laurenmessemer <lauren@mesmerize.studio>
2025-04-15 12:35:01 -0500

    metrics votes user

--- routes/metricsRoutes.js
+++ routes/metricsRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { getVoteMetrics, getAvgVotesPerUser, getVotingUserPercentage } = require("../controllers/metricsController");
 
 router.get("/votes/average", getAvgVotesPerUser);
-router.get("/votes/:userId", getVoteMetrics);
 router.get("/votes/voting-user-percentage", getVotingUserPercentage);
+router.get("/votes/:userId", getVoteMetrics);
+
 
 module.exports = router;
