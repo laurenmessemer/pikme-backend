5b86e7b790ae5413c6b238764ac032651eeb508c
laurenmessemer <lauren@mesmerize.studio>
2025-04-15 13:09:10 -0500

    metrics votes user

--- routes/metricsRoutes.js
+++ routes/metricsRoutes.js
@@ -6,7 +6,7 @@ router.get("/votes/average", getAvgVotesPerUser);
 router.get("/votes/voting-user-percentage", getVotingUserPercentage);
 router.get("/votes/competing-user-percentage", getCompetingUserPercentage);
 router.get("/votes/currently-active-competing-users", getCurrentlyActiveCompetingUsers);
-router.get("/votes/competing-users", getIntervalBasedCompetingUsers);
+router.get("/votes/competing-users-by-interval", getCompetingUsersByInterval); // ✅
 router.get("/votes/:userId", getVoteMetrics);
 
 
