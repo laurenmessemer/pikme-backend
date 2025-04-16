fa9c92788e5346703f19a4106e7924f56622c336
laurenmessemer <lauren@mesmerize.studio>
2025-04-16 16:34:39 -0500

    metrics votes user

--- routes/metricsRoutes.js
+++ routes/metricsRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getVoteMetrics, getAvgVotesPerUser, getVotingUserPercentage, getCompetingUserPercentage, getCurrentlyActiveCompetingUsers, getIntervalBasedCompetingUsers, } = require("../controllers/metricsController");
 
-router.get("/votes/average", getAvgVotesPerUser);
-router.get("/votes/voting-user-percentage", getVotingUserPercentage);
-router.get("/votes/competing-user-percentage", getCompetingUserPercentage);
-router.get("/votes/currently-active-competing-users", getCurrentlyActiveCompetingUsers);
-router.get("/votes/competing-users-by-interval", getIntervalBasedCompetingUsers); // ✅
-router.get("/votes/:userId", getVoteMetrics);
+// Import each controller function explicitly
+const metricsController = require("../controllers/metricsController");
 
+router.get("/votes/average", metricsController.getAvgVotesPerUser);
+router.get("/votes/voting-user-percentage", metricsController.getVotingUserPercentage);
+router.get("/votes/competing-user-percentage", metricsController.getCompetingUserPercentage);
+router.get("/votes/currently-active-competing-users", metricsController.getCurrentlyActiveCompetingUsers);
+router.get("/votes/competing-users-by-interval", metricsController.getIntervalBasedCompetingUsers);
+router.get("/votes/:userId", metricsController.getVoteMetrics);
 
 module.exports = router;
