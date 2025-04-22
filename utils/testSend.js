347b615255dc86e832e41500fde6b66b27226639
laurenmessemer <lauren@mesmerize.studio>
2025-04-22 11:39:36 -0500

    update email

--- utils/testSend.js
+++ utils/testSend.js
@@ -1,24 +1,24 @@
-const mailchimp = require("@mailchimp/mailchimp_transactional")("md-ghenRxKxpCsZ-VeELfh-nQ");
-console.log("📛 API Key Loaded:", "md-ghenRxKxpCsZ-VeELfh-nQ" ? "✅ OK" : "❌❌ MISSING");
+// const mailchimp = require("@mailchimp/mailchimp_transactional")("md-ghenRxKxpCsZ-VeELfh-nQ");
+// console.log("📛 API Key Loaded:", "md-ghenRxKxpCsZ-VeELfh-nQ" ? "✅ OK" : "❌❌ MISSING");
 
-const sendConfirmationEmail = async () => {
-  const toEmail = "hello@playpikme.com";
+// const sendConfirmationEmail = async () => {
+//   const toEmail = "hello@playpikme.com";
 
-  try {
-    const response = await mailchimp.messages.send({
-      message: {
-        from_email: "hello@playpikme.com",
-        from_name: "PikMe",
-        to: [{ email: toEmail, type: "to" }],
-        subject: "Test Email from PikMe",
-        text: "This is a plain text test email.",
-      },
-    });
+//   try {
+//     const response = await mailchimp.messages.send({
+//       message: {
+//         from_email: "hello@playpikme.com",
+//         from_name: "PikMe",
+//         to: [{ email: toEmail, type: "to" }],
+//         subject: "Test Email from PikMe",
+//         text: "This is a plain text test email.",
+//       },
+//     });
 
-    console.log("✅ Sent!", response);
-  } catch (err) {
-    console.error("❌ Failed to send:", err.response?.data || err);
-  }
-};
+//     console.log("✅ Sent!", response);
+//   } catch (err) {
+//     console.error("❌ Failed to send:", err.response?.data || err);
+//   }
+// };
 
-sendConfirmationEmail();
+// sendConfirmationEmail();
